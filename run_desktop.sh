833f5399e34bab3efb8d12623a58fb4748de0e62
Alex Harvey <alex.harvey@workwave.com>
2026-02-27 15:31:16 +1100

    chore: remove TUI-era shell scripts and examples

--- run_desktop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Building Dioxus Desktop App..."
-cargo build --release --bin logs-parser-desktop
-
-echo ""
-echo "Starting desktop application..."
-./target/release/logs-parser-desktop
